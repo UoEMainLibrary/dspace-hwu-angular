acb171e9a932640b38966dff4b8c190e9c23a966
John Pinto <jpinto@newington.ed.ac.uk>
2023-07-11 08:55:58 +0100

    HWU theme added to Dspace-7_x.

--- src/app/info/info-routing-paths.ts
+++ src/app/info/info-routing-paths.ts
@@ -3,6 +3,11 @@ import { getInfoModulePath } from '../app-routing-paths';
 export const END_USER_AGREEMENT_PATH = 'end-user-agreement';
 export const PRIVACY_PATH = 'privacy';
 export const FEEDBACK_PATH = 'feedback';
+// CUSTOMISED
+export const ABOUT_PATH = 'about';
+export const ACCESSIBILITY_PATH = 'accessibility';
+export const COPYRIGHT_PATH = 'copyright';
+export const ORGANISED_PATH = 'organised';
 
 export function getEndUserAgreementPath() {
     return getSubPath(END_USER_AGREEMENT_PATH);
@@ -14,6 +19,23 @@ export function getPrivacyPath() {
 
 export function getFeedbackPath() {
     return getSubPath(FEEDBACK_PATH);
+}
+
+//CUSTOMISED
+export function getAboutPath() {
+  return getSubPath(ABOUT_PATH);
+}
+
+export function getAccessibilityPath() {
+  return getSubPath(ACCESSIBILITY_PATH);
+}
+
+export function getCopyrightPath() {
+  return getSubPath(COPYRIGHT_PATH);
+}
+
+export function getOrganisedPath() {
+  return getSubPath(ORGANISED_PATH);
 }
 
 function getSubPath(path: string) {
--- src/app/info/info-routing.module.ts
+++ src/app/info/info-routing.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { I18nBreadcrumbResolver } from '../core/breadcrumbs/i18n-breadcrumb.resolver';
-import { PRIVACY_PATH, END_USER_AGREEMENT_PATH, FEEDBACK_PATH } from './info-routing-paths';
+import {
+  PRIVACY_PATH, END_USER_AGREEMENT_PATH,
+  FEEDBACK_PATH, ABOUT_PATH, ACCESSIBILITY_PATH, COPYRIGHT_PATH, ORGANISED_PATH
+} from './info-routing-paths';
 import { ThemedEndUserAgreementComponent } from './end-user-agreement/themed-end-user-agreement.component';
 import { ThemedPrivacyComponent } from './privacy/themed-privacy.component';
 import { ThemedFeedbackComponent } from './feedback/themed-feedback.component';
 import { FeedbackGuard } from '../core/feedback/feedback.guard';
 import { environment } from '../../environments/environment';
 
+// CUSTOMISED
+import { ThemedAboutComponent } from './about/themed-about.component';
+import { ThemedAccessibilityComponent } from './accessibility/themed-accessibility.component';
+import { ThemedCopyrightComponent } from './copyright/themed-copyright.component';
+import { ThemedOrganisedComponent } from './organised/themed-organised.component';
 
 const imports = [
   RouterModule.forChild([
@@ -42,6 +50,46 @@ const imports = [
           data: { title: 'info.privacy.title', breadcrumbKey: 'info.privacy' }
         }
       ]));
+  
+  imports.push(
+    RouterModule.forChild([
+      {
+        path: ABOUT_PATH,
+        component: ThemedAboutComponent,
+        resolve: { breadcrumb: I18nBreadcrumbResolver },
+        data: { title: 'info.about.title', breadcrumbKey: 'info.about' }
+      }
+    ]));
+
+  imports.push(
+    RouterModule.forChild([
+      {
+        path: ACCESSIBILITY_PATH,
+        component: ThemedAccessibilityComponent,
+        resolve: { breadcrumb: I18nBreadcrumbResolver },
+        data: { title: 'info.accessibility.title', breadcrumbKey: 'info.accessibility' }
+      }
+    ]));
+
+  imports.push(
+    RouterModule.forChild([
+      {
+        path: COPYRIGHT_PATH,
+        component: ThemedCopyrightComponent,
+        resolve: { breadcrumb: I18nBreadcrumbResolver },
+        data: { title: 'info.copyright.title', breadcrumbKey: 'info.copyright' }
+      }
+    ]));
+
+  imports.push(
+    RouterModule.forChild([
+      {
+        path: ORGANISED_PATH,
+        component: ThemedOrganisedComponent,
+        resolve: { breadcrumb: I18nBreadcrumbResolver },
+        data: { title: 'info.organised.title', breadcrumbKey: 'info.organised' }
+      }
+    ]));
   }
 
 @NgModule({
